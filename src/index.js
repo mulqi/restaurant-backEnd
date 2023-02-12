9cca4bbdf36a02ac3395658b468806f55f67385f
AriqFaridzki <ariqf35@gmail.com>
2023-02-12 16:40:12 +0700

    test branching

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send(' INI dari ariq :>');
+  res.send(' INI dari ariq 2 ke test branch :>');
 });
 
 app.listen(port, () => {
