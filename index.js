7777bef90b44ffec4d10f6a29994bad504c51143
beby-chan <125166833+beby-chan@users.noreply.github.com>
2023-02-19 20:32:52 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express()
+const port = 3000
+'git clone https://github.com/mulqi/restaurant-backEnd'
+const mysql = require ('mysql')
+const connection = mysql.createConnection ({
+  host: '109.106.253.140',
+  user: 'u1579227_kelompok_2',
+  database: 'u1579227_kelompok_2',
+  password: 'boba_kelompok_2'
+})
+
+connection.connect()
+connection.query('SELECT * from menu', (err, rows, fields) => {
+  if (err) throw err
+
+  console.log('The result is: ', rows)
+})
+
+connection.end()
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+// -> tampil data makanan 
+// -> database 
+// -> inquiry 
+// -> filter makanan yang masih tersedia => query (SELECT * lalala)
+app.get('/get-menu', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/get-menu', (params) => {
+    //
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
